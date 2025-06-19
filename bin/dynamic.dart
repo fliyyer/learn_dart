ca6475bc413ba557f24cf5dda64817c336a51c95
user.email <rahmatcube15@gmail.com>
2025-06-19 14:45:34 +0700

    Tipe data Dynamic

---
+++ bin/dynamic.dart
@@ -0,0 +1,10 @@
+void main() {
+  dynamic random = "Hallo";
+  print(random);
+  random = 10;
+  print(random);
+  random = true;
+  print(random);
+}
+
+// Tipe data dynamic bisa menampung semua tipe data
