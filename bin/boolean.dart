eccd2f5ceb2cf17808c40e11de9317ac0da2ab73
user.email <rahmatcube15@gmail.com>
2025-06-19 13:49:48 +0700

    boolean

---
+++ bin/boolean.dart
@@ -0,0 +1,13 @@
+void main() {
+  bool isLoading = true;
+  print('Maka isLoading bernilai: $isLoading');
+
+  bool isLogin = false;
+  print('Maka isLogin bernilai: $isLogin');
+}
+
+/*
+result
+Maka isLoading bernilai: true
+Maka isLogin bernilai: false
+*/
