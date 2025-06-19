783ff8655dc991f00e80f0519925d01d603690aa
user.email <rahmatcube15@gmail.com>
2025-06-19 14:42:42 +0700

    Tipe data Map

---
+++ bin/map.dart
@@ -0,0 +1,5 @@
+void main() {
+  Map<String, int> ages = {"Budi": 30, "Agus": 25, "Kiwil": 15};
+  print(ages); //menampilkan seluruh isi map
+  print(ages["Budi"]); //menampilkan isi map dengan key tertentu
+}
