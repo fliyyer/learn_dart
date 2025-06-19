72dbce2c68d6868dbe2d77816b8be8616151aac1
user.email <rahmatcube15@gmail.com>
2025-06-19 11:18:50 +0700

    tipe data number

---
+++ bin/number.dart
@@ -0,0 +1,21 @@
+void main() {
+  int age = 22;
+  double ipk = 3.78;
+
+  print("Umur saya $age tahun");
+  print("IPK saya $ipk");
+
+  // NUM = Tipe data number yang bisa menerima nilai int dan double
+  num number = 20;
+  print(number);
+  number = 20.5;
+  print(number);
+}
+
+/*
+result
+Umur saya 22 tahun
+IPK saya 3.78
+20
+20.5 
+*/
