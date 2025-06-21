dd8069ab707ca3846b1cf1d88f80401c7a7f5594
user.email <user.name>
2025-06-21 10:19:22 +0700

    for loops

---
+++ bin/for_loop.dart
@@ -0,0 +1,10 @@
+void main() {
+  for (var i = 1; i <= 10; i++) {
+    print("Angka ke-$i");
+  }
+
+  List<String> names = ["Rahmat", "Hidayat", "Budi"];
+  for (var name in names) {
+    print(name);
+  }
+}
