497e69c55c7279b83858e662324769c6a1cfeacb
user.email <user.name>
2025-06-21 10:11:15 +0700

    swirch case

---
+++ bin/switch_case.dart
@@ -0,0 +1,21 @@
+void main() {
+  var number = 100;
+
+  switch (number) {
+    case 1:
+      print("Satu");
+      break;
+    case 2:
+      print("Dua");
+      break;
+    case 3:
+      print("Tiga");
+      break;
+    case 4:
+      print("Empat");
+      break;
+    default:
+      print("Tidak ada yang cocok");
+      break;
+  }
+}
